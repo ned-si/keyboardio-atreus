b5f81f74c58301136a2297be744ae9a57a5b8234
nedsi <nedsi@pm.me>
2021-12-07 00:37:34 +0100

    initial commit

---
+++ rules.mk
@@ -0,0 +1,10 @@
+ENCODER_ENABLE = no       # Enable the use of one or more encoders
+TAP_DANCE_ENABLE = yes     # Activate tap dance
+
+COMBO_ENABLE = yes 				 # Enable combos
+NKRO_ENABLE = yes 				 # Enable NKRO: any number of key-presses at once
+LTO_ENABLE = yes  				 # Enable Link Time Optimization
+MOUSEKEY_ENABLE = no      # Enable mouse keys
+EXTRAKEY_ENABLE = yes      # Enable extra keys
+
+EXTRFLAGS += -flto
